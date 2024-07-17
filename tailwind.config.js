85136a9328d263eaf0303179632b8426b05e3ad6
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-07-17 10:27:01 +0600

    navbar made responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "ms": "320px",
+        "mm": "355px",
+        "ml": "425px",
+        "14i": "1360px",
+        "15i": "1500px"
+      }
+    },
   },
   plugins: [],
 }
