7d614dfc93edac96e3cd197efbcbc83b03e5ee14
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-08-15 22:21:33 +0600

    font family added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,11 @@ module.exports = {
         "ml": "425px",
         "14i": "1360px",
         "15i": "1500px"
-      }
+      },
+      fontFamily: {
+        arc: ['Archivo', 'sans-serif'],
+        // abc: ["Grey Qo", 'cursive']
+      },
     },
   },
   plugins: [],
